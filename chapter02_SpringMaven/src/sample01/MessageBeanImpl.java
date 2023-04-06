7c23e11111060fe1e8d4c28fa34ee444d5f26c55
dodam24 <dodam0724@gmail.com>
2023-04-06 18:44:20 +0900

    Spring Maven

--- chapter02_SpringMaven/src/sample01/MessageBeanImpl.java
+++ chapter02_SpringMaven/src/sample01/MessageBeanImpl.java
@@ -1,32 +1,34 @@
 package sample01;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-@RequiredArgsConstructor
+//@Component
 public class MessageBeanImpl implements MessageBean {
-	@NonNull
 	private String fruit;
-	@Setter
 	private int cost, qty;
 	
-	
 	//Constructor Injection - fruit 생성자 이용
-//	public MessageBeanImpl(String fruit) {
-//		super();
-//		this.fruit = fruit;
-//	}
+	public MessageBeanImpl(@Value("사과")String fruit) { //lombok이 아닌 spring으로 import
+		super();
+		this.fruit = fruit;
+	}
 	
-	//Setter Injection - cost, qty setter 이용
-//	public void setFruit(int cost) {
-//		this.cost = cost;
-//	}
-//
-//	public void setQty(int qty) {
-//		this.qty = qty;
-//	}
-	
+	@Autowired
+	//Setter Injection - cost, qty는 setter 이용
+	public void setCost(@Value("5000")int cost) {
+		this.cost = cost;
+	}
+
+	@Autowired
+	public void setQty(@Value("3")int qty) {
+		this.qty = qty;
+	}
 	
 	@Override
 	public void sayHello() {
